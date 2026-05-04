36c06bb2bb1e25e4024c19ac58bc3d8345b8874a
Song Zhenbo <yizhimao0111@gmail.com>
2026-05-04 22:55:51 +0800

    ARM: dts: defconfig: 添加对RocketCat开发板的初始支持
    基于NXP官方的BSP完成对RocketCat开发板的初步支持
    实现内核到命令行的启动
    
    Signed-off-by: Song Zhenbo <yizhimao0111@gmail.com>

--- imx6ull-rocketcat-emmc.sh
+++ imx6ull-rocketcat-emmc.sh
@@ -3,5 +3,8 @@
 set -e
 
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_v7_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_rocketcat_emmc_defconfig
 make HOSTCFLAGS="-fcommon" ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j$(nproc)
+
+cp arch/arm/boot/zImage ~/Bsp-Project/nfs/ -f
+cp arch/arm/boot/dts/imx6ull-rocketcat-emmc.dtb ~/Bsp-Project/nfs/ -f
